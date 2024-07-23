f18baa06257ab581635ba8fa065e1622bed56083
soumnayk <soumyadeep.nayak@justdial.com>
2024-07-23 12:00:21 +0530

    lock and provider

--- app/reducers/GlobalProvider.tsx
+++ app/reducers/GlobalProvider.tsx
@@ -3,7 +3,11 @@
 import { Provider } from "react-redux"
 import { store } from "../store"
 
-export function GlobalProvider({children}){
+interface GlobalProviderProps {
+    children: React.ReactNode;
+}
+
+export function GlobalProvider({ children }: GlobalProviderProps){
     return (
         <Provider store={store}>
             {children}
